323b37ad0e5e36faf2cfa68423d609fcaae79218
srn <salrio74@gmail.com>
2024-03-01 22:20:19 +0100

    feat(spring-rest): sprint rest test first commit

---
+++ spring/cashcard/src/test/java/example/cashcard/CashCardJsonTest.java
@@ -0,0 +1,12 @@
+package example.cashcard;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CashCardJsonTest {
+
+   @Test
+   void myFirstTest() {
+      assertThat(42).isEqualTo(42);
+   }
+}
